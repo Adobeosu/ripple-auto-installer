6ef0ebe83889a798469e2066b333efbcbeaa3151
Adobeosu <61052022+Adobeosu@users.noreply.github.com>
2020-11-30 00:50:19 -0500

    e

--- installer.sh
+++ installer.sh
@@ -64,7 +64,7 @@ echo -e "\e[1;36mInstalling dependencies..."
 echo -e "\e[\033[0m"
 sleep 1
 apt-get update
-sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev -y	 
+sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev -y	 
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 apt-get install python3 python3-dev -y
